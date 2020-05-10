e927a4c0178540bfab6160ebd125060ede6e8d3e
elvadanisa <elvadanisa24@gmail.com>
2020-05-10 22:24:34 +0700

    projek

---
+++ home.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Tugas Pemrograman Web</title>
+	<link rel="stylesheet" href="stylee.css">
+</head>
+<body>
+
+	<div class="container">
+		<div class="header">
+			<h1 class="judul">WELCOME TO PDO CRUD!</h1>
+		</div>
+		<div class="hero">
+			<img src="heroo.png" width="800px" height="250px" ;>
+			<div class="content">
+				<h2>Tutorial PDO CRUD</h2>
+				<p class="penulis">ditulis oleh <a href="#">elvadanisa</a>.pada 1 Mei 2020</p>
+
+				<p>Selamat datang di PDO CRUD, dalam tutorial ini kita akan melanjutkan pembahasan mengenai seri tutorial php, kita akan belajar membuat CRUD dengan OOP dan PDO MySQL di PHP.
+
+				Melanjutkan pembahasan PDO di php, kali ini kita akan mulai untuk belajar bagaimana cara membuat crud sederhana dengan menggunakan OOP dan PDO MySQL di PHP.</p>
+
+				
+			</div>
+			<div class="footer">
+				<p class="copy">copyright 2020. elvadanisa</p>
+			</div>
+		</div>
+	</div>
+</body>
+</html>
